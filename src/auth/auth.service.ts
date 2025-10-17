2af6bc0b74a08d16f843cc6b93281d7eb8b04c2f
internethero00 <egormix1703@gmail.com>
2025-10-18 00:38:28 +0300

    + auth service update

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,13 +4,19 @@ import { JwtService } from '@nestjs/jwt'
 import { PrismaService } from '../prisma.service'
 import { AuthDto } from './dto/auth.dto'
 import { UserDB } from '../types/user_db'
+import { Response } from 'express'
+import { ConfigService } from '@nestjs/config'
 
 @Injectable()
 export class AuthService {
 
+	EXPIRE_DAY_REFRESH_TOKEN = 1
+	REFRESH_TOKEN_NAME = 'refreshToken'
+
 	constructor(private jwt: JwtService,
 				private userService: UserService,
-				private prisma: PrismaService) {
+				private prisma: PrismaService,
+				private configService: ConfigService) {
 	}
 
 	async login(dto: AuthDto) {
@@ -51,6 +57,31 @@ export class AuthService {
 		if (!user) throw new NotFoundException('User not found')
 
 		return user
+	}
 
+	addRefreshTokenToResponse(res: Response, refreshToken: string) {
+		const expiresIn = new Date()
+		expiresIn.setDate(expiresIn.getDate() + this.EXPIRE_DAY_REFRESH_TOKEN)
+
+		res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, {
+			expires: expiresIn,
+			httpOnly: true,
+			domain: this.configService.get('SERVER_DOMAIN'),
+			secure: true,
+			sameSite: 'none'
+			}
+		)
+	}
+
+	removeRefreshTokenFromResponse(res: Response) {
+
+		res.cookie(this.REFRESH_TOKEN_NAME, '', {
+				expires: new Date(0),
+				httpOnly: true,
+				domain: this.configService.get('SERVER_DOMAIN'),
+				secure: true,
+				sameSite: 'none'
+			}
+		)
 	}
 }
