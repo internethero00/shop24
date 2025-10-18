7ed62b53cc4239371676db2092db4ae2cc54a3c8
internethero00 <egormix1703@gmail.com>
2025-10-18 17:24:12 +0300

    + google/callback, yandex/callback

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,27 +1,30 @@
 import {
   Body,
-  Controller,
+  Controller, Get,
   HttpCode,
   Post,
   Req,
   Res,
-  UnauthorizedException,
+  UnauthorizedException, UseGuards,
   UsePipes,
   ValidationPipe
 } from '@nestjs/common'
 import { AuthService } from './auth.service';
 import type { AuthDto } from './dto/auth.dto'
 import type { Request, Response } from 'express'
+import { AuthGuard } from '@nestjs/passport'
+import * as process from 'node:process'
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {
+  }
 
   @UsePipes(new ValidationPipe())
   @HttpCode(200)
   @Post('login')
-  async login(@Body() dto: AuthDto, @Res({passthrough: true}) res: Response) {
-    const {refreshToken, ...response} = await this.authService.login(dto)
+  async login(@Body() dto: AuthDto, @Res({ passthrough: true }) res: Response) {
+    const { refreshToken, ...response } = await this.authService.login(dto)
     this.authService.addRefreshTokenToResponse(res, refreshToken)
 
     return response
@@ -30,8 +33,8 @@ export class AuthController {
   @UsePipes(new ValidationPipe())
   @HttpCode(200)
   @Post('register')
-  async register(@Body() dto: AuthDto,  @Res({passthrough: true}) res: Response) {
-    const {refreshToken, ...response} = await this.authService.register(dto)
+  async register(@Body() dto: AuthDto, @Res({ passthrough: true }) res: Response) {
+    const { refreshToken, ...response } = await this.authService.register(dto)
     this.authService.addRefreshTokenToResponse(res, refreshToken)
     return response
   }
@@ -39,15 +42,49 @@ export class AuthController {
   @UsePipes(new ValidationPipe())
   @HttpCode(200)
   @Post('login/access-token')
-  async getNewTokens(@Req() req: Request,  @Res({passthrough: true}) res: Response) {
+  async getNewTokens(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
     const refreshTokenFromCookies = req.cookies[this.authService.REFRESH_TOKEN_NAME]
     if (!refreshTokenFromCookies) {
       this.authService.removeRefreshTokenFromResponse(res)
       throw new UnauthorizedException('not valid refresh token')
     }
-    const {refreshToken, ...response} = await this.authService.getNewTokens(refreshTokenFromCookies as string)
+    const { refreshToken, ...response } = await this.authService.getNewTokens(refreshTokenFromCookies as string)
     this.authService.addRefreshTokenToResponse(res, refreshToken)
     return response
+  }
+
+  @HttpCode(200)
+  @Post('logout')
+  async logout(@Res({ passthrough: true }) res: Response) {
+    this.authService.removeRefreshTokenFromResponse(res)
+    return true
+  }
+
+  @Get('google')
+  @UseGuards(AuthGuard('google'))
+  async googleAuth() {
+  }
+
+  @Get('google/callback')
+  @UseGuards(AuthGuard('google'))
+  async googleAuthCallback(@Req() req: any, @Res({ passthrough: true }) res: Response) {
+    const { refreshToken, ...response } = await this.authService.validateOAuthLogin(req)
+    this.authService.addRefreshTokenToResponse(res, refreshToken)
+    return res.redirect(`${process.env['CLIENT_URL']}/dashboard?accessToken=${response.accessToken}`)
+  }
+
+
+  @Get('yandex')
+  @UseGuards(AuthGuard('yandex'))
+  async yandexAuth() {
+  }
+
+  @Get('yandex/callback')
+  @UseGuards(AuthGuard('yandex'))
+  async yandexAuthCallback(@Req() req: any, @Res({ passthrough: true }) res: Response) {
+    const { refreshToken, ...response } = await this.authService.validateOAuthLogin(req)
+    this.authService.addRefreshTokenToResponse(res, refreshToken)
+    return res.redirect(`${process.env['CLIENT_URL']}/dashboard?accessToken=${response.accessToken}`)
   }
 
 }
