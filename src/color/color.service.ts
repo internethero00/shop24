1a996e855c091f16065d09bab2a1b104edefb633
internethero00 <egormix1703@gmail.com>
2025-10-21 21:13:37 +0300

    color update

--- src/color/color.service.ts
+++ src/color/color.service.ts
@@ -1,4 +1,56 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { PrismaService } from '../prisma.service'
+import { ColorDto } from './dto/color.dto'
 @Injectable()
-export class ColorService {}
+export class ColorService {
+	constructor(private prisma: PrismaService) {}
+
+	async getByStoreId(storeId: string) {
+		return this.prisma.colore.findMany({
+			where: {
+				storeId
+			}
+		})
+	}
+
+	async getById(id: string) {
+		const color = await this.prisma.colore.findUnique({
+			where: {
+				id
+			}
+		})
+
+		if (!color) throw new NotFoundException('Color not found')
+
+		return color
+	}
+
+	async create(storeId: string,dto: ColorDto) {
+		return this.prisma.colore.create({
+			data: {
+				...dto,
+				storeId
+			},
+		})
+	}
+
+	async update(id: string, dto: ColorDto) {
+		await this.getById(id)
+		return this.prisma.colore.update({
+			where: {
+				id,
+			},
+			data: {
+				...dto
+			},
+		})
+	}
+	async delete(id: string) {
+		await this.getById(id)
+		return this.prisma.colore.delete({
+			where: {
+				id
+			}
+		})
+	}
+}
